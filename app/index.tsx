eecee72c4aec551c081235d8eadde7c584346fda
Amiya Ranjan Das <ranjandass13@gmail.com>
2025-08-24 16:27:39 +0530

    notification added

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,53 @@
-import { StyleSheet } from "react-native";
+import { Button, Platform, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Notifications from "expo-notifications";
+import { SchedulableTriggerInputTypes } from "expo-notifications";
+import { useEffect } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export default function HomeScreen() {
+  useEffect(() => {
+    // Create channel for Android
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+  }, []);
+
+  const onDisplayNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Hello 👋",
+        body: "This is a local notification!",
+      },
+      trigger: {
+        type: SchedulableTriggerInputTypes.TIME_INTERVAL,
+        seconds: 1,
+      }, // show after 1 second
+    });
+  };
   return (
     <SafeAreaView style={styles.container}>
+      <Button
+        title="Display Notification"
+        onPress={() => {
+          onDisplayNotification();
+        }}
+      />
       <WebView
         source={{ uri: "https://amiyadas.github.io/renewly/" }}
         style={{ flex: 1 }}
